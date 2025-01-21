7afab82b66b365e88d081355a2d0134d47807247
achraf <achrafbengarin2002@gmail.com>
2025-01-21 22:16:42 +0100

    add info task

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views.tudolist_views import TudoListView
 
 urlpatterns = [
-    path('',TudoListView.TaskList.as_view(),name="home")
+    path('',TudoListView.TaskList.as_view(),name="home"),
+    path('task/<int:pk>/',TudoListView.DetailList.as_view(),name="task"),
 ]
--- base/views/tudolist_views.py
+++ base/views/tudolist_views.py
@@ -2,9 +2,16 @@ from django.views import View
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from ..models.TaskModels import Task
 
 class TudoListView(View):
     class TaskList(ListView):  
         model = Task
         context_object_name= "tasklist"
+        template_name="base/tasklist.html"
+        
+    class DetailList(DetailView):
+        model = Task
+        context_object_name= "taskdetail"
+        template_name="base/taskdetail.html"    
